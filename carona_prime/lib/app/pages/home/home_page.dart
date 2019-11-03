f76645c28d486dd4eb5e705e145b8f24a19cc1bd
Pedro Henrique Barbiero <pedrohenriquebarbiero@hotmail.com>
2019-11-03 18:13:56 -0500

    login com numero de telefone

--- carona_prime/lib/app/pages/home/home_page.dart
+++ carona_prime/lib/app/pages/home/home_page.dart
@@ -11,8 +11,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Carona Prime')),      
+      appBar: AppBar(title: Text('Carona Prime')),
       drawer: DefaultDrawer(),
+      // bottomNavigationBar: BottomAppBar(
+      //     child: Row(
+      //   mainAxisSize: MainAxisSize.max,
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   children: <Widget>[
+      //     FlatButton(
+      //       child: Text(
+      //         'Voltar',
+      //         style: TextStyle(
+      //           color: Colors.white,
+      //           fontSize: 20.0,
+      //         ),
+      //       ),
+      //       onPressed: () {
+      //         Navigator.push(context,
+      //             MaterialPageRoute(builder: (context) => WelcomePage()));
+      //       },
+      //     ),
+      //   ],
+      // )),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
