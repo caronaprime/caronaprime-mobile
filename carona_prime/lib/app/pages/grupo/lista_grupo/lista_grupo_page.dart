8714f1be88553bc7d5a12b40c9c113af33d24b88
Pedro Henrique Barbiero <pedrohenriquebarbiero@hotmail.com>
2020-02-16 19:29:07 -0400

    implementacao do pull to refresh na lista de grupos

--- carona_prime/lib/app/pages/grupo/lista_grupo/lista_grupo_page.dart
+++ carona_prime/lib/app/pages/grupo/lista_grupo/lista_grupo_page.dart
@@ -4,24 +4,31 @@ import 'package:carona_prime/app/pages/grupo/novo_grupo/novo_grupo_page.dart';
 import 'package:carona_prime/app/shared/widgets/default_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class ListaGrupoPage extends StatelessWidget {
+class ListaGrupoPage extends StatefulWidget {
+  @override
+  _ListaGrupoPageState createState() => _ListaGrupoPageState();
+}
+
+class _ListaGrupoPageState extends State<ListaGrupoPage> {
+  @override
+  void initState() {
+    controller.carregarGrupos();
+    super.initState();
+  }
+
   final controller = ListaGrupoController();
+  List<String> items = [];
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   Widget build(BuildContext context) {
-    controller.carregarGrupos();
     return Scaffold(
       drawer: DefaultDrawer(),
       appBar: AppBar(
         title: Text("Grupos"),
-        // actions: <Widget>[
-        //   IconButton(
-        //     icon: Icon(Icons.notifications),
-        //     onPressed: () => Navigator.of(context).push(
-        //         MaterialPageRoute(builder: (context) => NotificacoesPage())),
-        //   )
-        // ],
       ),
       body: Observer(builder: (_) {
         if (controller.gruposResponse == null ||
@@ -33,46 +40,80 @@ class ListaGrupoPage extends StatelessWidget {
                 : CircularProgressIndicator(),
           ));
 
-        return ListView(
-          children: controller.gruposResponse
-              .map((grupo) => ListTile(
-                    onTap: () => Navigator.of(context).push<bool>(
-                        MaterialPageRoute(
-                            builder: (context) => DetalhesGrupoPage(grupo.id))),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text(
-                                    "Tem certeza que deseja sair do do grupo ${grupo.nome}?"),
-                                actions: <Widget>[
-                                  FlatButton(
-                                    child: Text("Não"),
-                                    onPressed: () =>
-                                        Navigator.of(context).pop(),
-                                  ),
-                                  FlatButton(
-                                    child: Text("Sim"),
-                                    onPressed: () async {
-                                      await controller.sairDoGrupo(grupo.id);
-                                      if (Navigator.of(context).canPop()) {
-                                        Navigator.of(context).pop();
-                                      }
-                                    },
-                                  )
-                                ],
-                              );
-                            });
-                      },
-                    ),
-                    title: Text(grupo.nome),
-                    subtitle:
-                        Text("${grupo.partida.nome} - ${grupo.destino.nome}"),
-                  ))
-              .toList(),
+        return SmartRefresher(
+          enablePullDown: true,
+          header: MaterialClassicHeader(),
+          footer: CustomFooter(
+            builder: (BuildContext context, LoadStatus mode) {
+              Widget body;
+              if (mode == LoadStatus.idle) {
+                body = Text("Puxe para atualizar");
+              } else if (mode == LoadStatus.loading) {
+                body = CircularProgressIndicator();
+              } else if (mode == LoadStatus.failed) {
+                body = Text("Atualização falhou. Clique para tentar novamente");
+              } else if (mode == LoadStatus.canLoading) {
+                body = Text("Pronto para carregar mais");
+              } else {
+                body = Text("Sem mais registros");
+              }
+              return Container(
+                height: 55.0,
+                child: Center(child: body),
+              );
+            },
+          ),
+          controller: _refreshController,
+          onRefresh: () async {
+            await controller.carregarGrupos();
+            _refreshController.refreshCompleted();
+          },
+          onLoading: () async {
+            await controller.carregarGrupos();
+            _refreshController.loadComplete();
+          },
+          child: ListView(
+            children: controller.gruposResponse
+                .map((grupo) => ListTile(
+                      onTap: () => Navigator.of(context).push<bool>(
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  DetalhesGrupoPage(grupo.id))),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(
+                                      "Tem certeza que deseja sair do do grupo ${grupo.nome}?"),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text("Não"),
+                                      onPressed: () =>
+                                          Navigator.of(context).pop(),
+                                    ),
+                                    FlatButton(
+                                      child: Text("Sim"),
+                                      onPressed: () async {
+                                        await controller.sairDoGrupo(grupo.id);
+                                        if (Navigator.of(context).canPop()) {
+                                          Navigator.of(context).pop();
+                                        }
+                                      },
+                                    )
+                                  ],
+                                );
+                              });
+                        },
+                      ),
+                      title: Text(grupo.nome),
+                      subtitle:
+                          Text("${grupo.partida.nome} - ${grupo.destino.nome}"),
+                    ))
+                .toList(),
+          ),
         );
       }),
       floatingActionButton: FloatingActionButton(
